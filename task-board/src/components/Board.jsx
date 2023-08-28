7a568a118b9fbb30247652817f7b07a724a0f5bf
Rahiful Islam <rahifulislam@gmail.com>
2023-08-28 14:30:33 +0600

    first commit

---
+++ task-board/src/components/Board.jsx
@@ -0,0 +1,13 @@
+// Board.js
+import React from 'react';
+import '../styles/Board.css';
+
+const Board = () => {
+    return (
+      <div className="board-container">
+        <h1>Card board</h1>
+      </div>
+    );
+  };
+
+export default Board;
